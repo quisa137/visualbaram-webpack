cac932e3870529ea379fe011df27e7d6b95e136b
SGcom <quisa137@gmail.com>
2016-05-09 11:49:58 +0900

    Dependancy upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 var path = require('path');
 var webpack = require('webpack');
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   target:"web",
-  entry: './web/main.js',
-  output: { path: __dirname, filename: 'web/bundle.js' },
+  entry: [
+    'webpack-dev-server/client?http://localhost:5000',
+    'webpack/hot/dev-server',
+    './web/main.jsx'
+  ],
+  output: { path: __dirname, filename: 'web/bundle.js',publicPath:'/web/'},
+  resolve: {
+    extensions: ['', '.js']
+  },
+  devtool: 'eval-source-map',
   module: {
     loaders: [
       {
         test: /.jsx?$/,
-        loader: 'babel-loader',
+        /*loaders: ['babel','babel-loader'],*/
+        loader:'babel-loader',
+        include: path.join(__dirname,'scripts'),
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
@@ -26,11 +36,11 @@ module.exports = {
   },
   plugins:[
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
     new webpack.ProvidePlugin({
       'Promise': 'bluebird', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
       'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
     }),
-    new webpack.NoErrorsPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve('./', 'index.html'),
       webpackDevServer: '/webpack-dev-server.js'
